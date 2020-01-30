7c6a783417c067705d620f9c747f259e34e76613
Jsinclairisto <jakflet@aol.com>
2020-01-30 13:33:51 -0500

    11th commit of flask blog. General clean up was done.

--- app/routes.py
+++ app/routes.py
@@ -28,21 +28,6 @@ def login():
 
     return render_template('login.html', title='Sign In', form=form)
 
-@app.route('/all')
-def temp_listings():
-
-    file_path = os.chdir('app/templates/')
-
-    print(file_path)
-    # temp_path = os.path.basename('/home/jakob/Desktop/flask-blog/app/')
-
-    files = os.listdir(file_path)
-    print(files)
-
-    return render_template('all.html', files=files)
-
-    #if (os.getcwd = os.curdir()):
-
     
 
 #generic page
@@ -50,7 +35,17 @@ def temp_listings():
 
 #input parameter name must match route parameter
 def render_page(view_name):
+    #Uses os to change targeted path
+    file_path = os.chdir('app/templates/')
+
+    
+    # temp_path = os.path.basename('/home/jakob/Desktop/flask-blog/app/')
+
+    #assigns file contents to variable. Now it can be read and displayed in the html file.
+    files = os.listdir(file_path)
+
+    return render_template('all.html', files=files)
     html = render_markdown(view_name + '.html')
-    return render_template_string(html, view_name = view_name)
+    return render_template_string(html, view_name=view_name, files=files)
 
 
