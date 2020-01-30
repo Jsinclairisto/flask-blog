8ac6fcd78e6896efcf4efeb4c197bd5963b11b57
Jsinclairisto <jakflet@aol.com>
2020-01-30 13:26:50 -0500

    10th commit of flask blog. Implemented the /all route

--- app/blog_helpers.py
+++ app/blog_helpers.py
@@ -1,11 +1,14 @@
 from markdown import markdown
 from flask_wtf import FlaskForm
+from flask import render_template
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+from wtforms.validators import Length
+
 import os
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=6, max=25)])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign in')
@@ -22,4 +25,3 @@ def render_markdown(file_name, dir_path = 'app/templates'):
         html = html_file.read()
         html = markdown(html)
     return html
-
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,13 @@
 #import certain functions into the global
 #namespace
 from app import app
+from os import walk
 from markdown import markdown
 from flask import render_template_string
 from flask import render_template
 from app.blog_helpers import render_markdown
 from app.blog_helpers import LoginForm
-
+import os
 #safe global import (okay to use)
 import flask
 
@@ -21,7 +22,28 @@ def home():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     form = LoginForm()
+
+    # if request.methpd == 'POST':
+    #     session['key'] = request.values['user_name']
+
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/all')
+def temp_listings():
+
+    file_path = os.chdir('app/templates/')
+
+    print(file_path)
+    # temp_path = os.path.basename('/home/jakob/Desktop/flask-blog/app/')
+
+    files = os.listdir(file_path)
+    print(files)
+
+    return render_template('all.html', files=files)
+
+    #if (os.getcwd = os.curdir()):
+
+    
 
 #generic page
 @app.route("/<view_name>")
@@ -30,4 +52,5 @@ def login():
 def render_page(view_name):
     html = render_markdown(view_name + '.html')
     return render_template_string(html, view_name = view_name)
+
 
