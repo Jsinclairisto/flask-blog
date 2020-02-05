71cd9981faa98bb29ffc8bea00c43834f9ab8f77
Jsinclairisto <jakflet@aol.com>
2020-02-05 16:06:22 -0500

    15th commit of flask blog. Still doing work on the edit route.

--- app/routes.py
+++ app/routes.py
@@ -2,30 +2,20 @@
 #namespace
 from app import app
 from os import walk
-from flask_user import roles_required
+from flask_user import roles_required, login_required
 from markdown import markdown
-from flask import render_template_string, render_template, flash, redirect, request, url_for
+from flask import render_template_string, render_template, flash, redirect, request
 from app.blog_helpers import render_markdown, LoginForm
+import urllib.request
 import os
 #safe global import (okay to use)
 import flask
-
-hasAccess = False
 
 #home page
 @app.route("/")
 def home():
     return render_template('index.html')
 
-
-@app.route('/edit/<edited_page_name>')   
-def edit(edited_page_name):
-    edit_page = render_markdown(edited_page_name + '.html')
-    print(hasAccess)
-    if(hasAccess == True):
-         return render_template_string(edit_page, edited_page_name = edited_page_name)
-    else:
-        return '<h1>YOOOOOOOOOO NOOOOOOOOOOO</h1>'
 
 #Success page. Directs here after form is submitted
 @app.route('/success')
@@ -66,16 +56,16 @@ def temp_listings():
     #combines base path with target path. This way, it will work with all users.
     #They'll have different base paths, but will have the same sub-path of '/app/templates'
     dest_path = base_path + '/app/templates'
-
     #assigns combo to file_path
     file_path = os.path.relpath(dest_path, base_path)
-
     files = os.listdir(file_path)
 
     return render_template('all.html', files=files)
 
+
+
 #generic page
-@app.route("/<view_name>")
+@app.route('/<view_name>')
 
 #input parameter name must match route parameter
 def render_page(view_name):
@@ -83,3 +73,32 @@ def render_page(view_name):
     print('YOOOOO IT WORKS AYYYYY')
     
     return render_template_string(html, view_name = view_name)
+
+@app.route('/edit/<edit_file>')   
+#@roles_required('Admin')
+def edit(edit_file):
+    
+
+    #edit_page = render_markdown(edited_page_name + '.html')
+    #base_path=temp_listings()
+    #final_page = render_markdown(edit_file + '.html')
+    fname = r"/home/jakob/Desktop/flask-blog/app/templates/all.html"
+    edit_file = open(fname, 'w')
+    return edit_file
+
+
+    contents = ""
+    with open('app/templates/' + final_page) as f:
+        for line in f.readlines():
+            contents += line  
+        return render_template('edit.html', contents=contents)
+    
+   
+
+    #log = open(base_path, "r")
+
+    # print(hasAccess)
+    # if(hasAccess == True):
+    #      return render_template_string(edit_page, edited_page_name = edited_page_name)
+    # else:
+    #     return '<h1>YOOOOOOOOOO NOOOOOOOOOOO</h1>'
